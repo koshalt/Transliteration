f2040f8bd0c501efd170ca2d13f9c464ce09d0c1
Koshal Thirumalai <kosh@koshal.org>
2014-03-13 16:25:36 -0700

    adding java files

---
+++ src/main/java/TransliterateApp.java
@@ -0,0 +1,13 @@
+/**
+ * Created by kosh on 3/13/14.
+ */
+public class TransliterateApp {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+        for(String current: HindiMapping.symbols){
+            System.out.println(current);
+        }
+    }
+}
