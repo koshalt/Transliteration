068c91f048353a1bdd1728deabfbd8d6cfd62258
Koshal Thirumalai <kosh@koshal.org>
2014-03-19 16:36:29 -0700

    Working set with no vowel marks yet

---
+++ src/main/java/mapping/CharacterMapping.java
@@ -0,0 +1,11 @@
+package mapping;
+
+import java.util.HashMap;
+
+/**
+ * Created by kosh on 3/17/14.
+ */
+public interface CharacterMapping {
+
+    public HashMap<String, String[]> getMapping();
+}
